ba03b680bc746534c8ecb72a5ae6055437659abb
rottenshorts <rottenshorts@tutanota.com>
2023-06-16 08:52:45 +0200

    added settings.txt

--- SlytherCord.py
+++ SlytherCord.py
@@ -1,40 +1,19 @@
 import discord
-from discord.ext import commands
+import os
+from settings import guild_id, bot_token
 
-# Create an instance of the bot
-bot = commands.Bot(command_prefix="!")
+client = discord.Client(intents=discord.Intents.all())
+session_id = os.urandom(8).hex()
 
-# Event: When the bot is ready and connected to the server
-@bot.event
+guild = client.get_guild(int(guild_id))
+channel = guild.create_text_channel(session_id)
+
+@client.event
 async def on_ready():
-    print(f"Bot connected as {bot.user.name}")
+    await channel.send("clinically online!")
+    
+@client.event
+async def on_message(message):
+    await channel.send("aight")
 
-# Command: Ping
-@bot.command()
-async def ping(ctx):
-    await ctx.send("Pong!")
-
-# Command: Say
-@bot.command()
-async def say(ctx, *, message):
-    await ctx.send(message)
-
-# Command: Kick
-@bot.command()
-@commands.has_permissions(kick_members=True)
-async def kick(ctx, member: discord.Member, *, reason="No reason provided."):
-    await member.kick(reason=reason)
-    await ctx.send(f"{member.display_name} has been kicked for {reason}.")
-
-# Event: When an error occurs during command execution
-@bot.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.CommandNotFound):
-        await ctx.send("Invalid command. Please try again.")
-    elif isinstance(error, commands.MissingPermissions):
-        await ctx.send("You don't have permission to use this command.")
-    else:
-        print(f"Error: {error}")
-
-# Replace 'YOUR_BOT_TOKEN' with your own bot token
-bot.run('YOUR_BOT_TOKEN')
+client.run(bot_token)
