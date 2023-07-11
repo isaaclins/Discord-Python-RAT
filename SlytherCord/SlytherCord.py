a0a1748bee3262c9f124d97987a7a9001f7f90d9
Isaac Lins <isaac.lins07@gmail.com>
2023-07-11 11:50:44 +0200

    Update SlytherCord.py

--- SlytherCord/SlytherCord.py
+++ SlytherCord/SlytherCord.py
@@ -2,18 +2,18 @@ import discord
 import sys
 from settings import *
 
-
 client = discord.Client(intents=discord.Intents.all())
+session_id = os.urandom(6).hex()
 
-channel_ids = {
-    'main': 1114528537093865494
-}
+guild = client.get_guild(int(guild_id))
+channel = guild.create_text_channel(session_id)
 
-
-
-
-@client.event 
-async def on_ready():  
-    await client.get_channel(channel_ids['main']).send('New PC session')
+@client.event
+async def on_ready():
+    await channel.send("clinically online!")
+    
+@client.event
+async def on_message(message):
+    await channel.send("aight")
 
 client.run(bot_token)
