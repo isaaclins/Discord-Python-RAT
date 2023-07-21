fb1cd29201589e444d84384e48336a828f5016b4
isaaclins-laptop <isaac.lins07@gmail.com>
2023-07-21 23:07:28 +0200

    refactored builder.py

--- main/Builder.py
+++ main/Builder.py
@@ -1,6 +1,5 @@
 import tkinter as tk
-from tkinter import filedialog
-from tkinter import messagebox
+from tkinter import filedialog, messagebox
 import os
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -35,40 +34,65 @@ def load_Settings():
         pass
 
 def compile_action():
-    bot_token = bot_token_input.get()
-    guild_id = guild_id_input.get()
+    try:
+        with open(Settings_file_path, "r") as file:
+            Settings = file.readlines()
+
+        for line in Settings:
+            if line.startswith("bot_token"):
+                bot_token = line.split("=")[1].strip().strip("'")
+
+            elif line.startswith("guild_id"):
+                guild_id = line.split("=")[1].strip().strip("'")
+
+    except FileNotFoundError:
+        messagebox.showerror("Error", "You haven't even applied ;C Enter the bot token and guild ID and press the Apply button")
+        pass
 
     if bot_token and guild_id:  # Check if both bot_token and guild_id are not empty
         # Display the warning message
-        messagebox.showerror("Compiling...", "compiling...")
+        messagebox.showerror("Compiling...", "Compiling...")
     else:
         # Clear the warning message if either bot_token or guild_id is empty
         messagebox.showerror("Error", "Both Bot Token and Guild ID must be filled.")
 
 app = tk.Tk()
-app.title("Settings")
-app.geometry("300x200")
+app.title("SlytherCord builder")
+app.geometry('700x620')
+app.resizable(True, True)
+app.configure(bg='#0A0A10')
+app.tk_setPalette(background='#0A0A10', foreground='white', activeBackground='#0A0A10', activeForeground='white')
 
-bot_token_label = tk.Label(app, text="Bot Token:")
-bot_token_label.pack()
-bot_token_input = tk.Entry(app)
-bot_token_input.pack()
+# Left Frame for labels
+left_frame = tk.Frame(app, bg='#0A0A10')
+left_frame.pack(side=tk.LEFT, padx=10, pady=10)
 
-guild_id_label = tk.Label(app, text="Guild ID:")
-guild_id_label.pack()
-guild_id_input = tk.Entry(app)
-guild_id_input.pack()
+# Right Frame for input fields and buttons
+right_frame = tk.Frame(app, bg='#0A0A10')
+right_frame.pack(side=tk.RIGHT, padx=10, pady=10)
 
-apply_button = tk.Button(app, text="Apply", command=apply_Settings)
-apply_button.pack()
+bot_token_label = tk.Label(left_frame, text="Bot Token:", fg='white', bg='#0A0A10')
+bot_token_label.pack(side=tk.TOP)
 
-load_button = tk.Button(app, text="Load", command=load_Settings)
-load_button.pack()
+guild_id_label = tk.Label(left_frame, text="Guild ID:", fg='white', bg='#0A0A10')
+guild_id_label.pack(side=tk.TOP)
 
-quit_button = tk.Button(app, text="Quit", command=app.quit)
-quit_button.pack()
+bot_token_input = tk.Entry(right_frame)
+bot_token_input.pack(side=tk.TOP)
 
-compile_button = tk.Button(app, text="Compile", command=compile_action)
-compile_button.pack()
+guild_id_input = tk.Entry(right_frame)
+guild_id_input.pack(side=tk.TOP)
+ 
+apply_button = tk.Button(left_frame, text="Apply", command=apply_Settings)
+apply_button.pack(side=tk.TOP, pady=10, padx=150)
+
+load_button = tk.Button(left_frame, text="Load", command=load_Settings)
+load_button.pack(side=tk.TOP, pady=10, padx=150)
+
+quit_button = tk.Button(right_frame, text="Quit", command=app.quit)
+quit_button.pack(side=tk.TOP, pady=10, padx=150)
+
+compile_button = tk.Button(right_frame, text="Compile", command=compile_action)
+compile_button.pack(side=tk.TOP, pady=10, padx=150)
 
 app.mainloop()
